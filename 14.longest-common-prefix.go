089ad5959ffa2e80066dcca11fa499d6cb3868d3
Hieu Tran <tr.kimhieu@gmail.com>
2024-06-26 11:21:28 +0000

    14.longest-common-prefix.go

---
+++ 14.longest-common-prefix.go
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=14 lang=golang
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+func longestCommonPrefix(strs []string) string {
+	p := strs[0]
+	for _, s := range strs {
+		i := 0
+		for ; i < len(s) && i < len(p) && p[i] == s[i]; i++ {
+		}
+		p = p[:i]
+	}
+	return p
+}
+
+// @lc code=end
+
