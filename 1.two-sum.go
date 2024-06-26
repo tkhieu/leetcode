32badf9c40779b021e8bba2f5baa74ca013d0178
Hieu Tran <tr.kimhieu@gmail.com>
2024-06-26 10:42:29 +0000

    1.two-sum.go

--- 1.two-sum.go
+++ 1.two-sum.go
@@ -8,12 +8,13 @@
 func twoSum(nums []int, target int) []int {
 	m := make(map[int]int)
 	for i, v := range nums {
-		if j, ok := m[target - v]; ok {
+		if j, ok := m[target-v]; ok {
 			return []int{j, i}
 		}
 		m[v] = i
 	}
 	return nil
 }
+
 // @lc code=end
 
